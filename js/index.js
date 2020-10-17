e620561d72bcb551a43b608a3d832fd2e2a30596
Tayyab30 <47396069+Tayyab30@users.noreply.github.com>
2020-10-16 22:01:14 -0700

    again update commit

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ window.requestAnimFrame = function () {
   };
 }();
 
-// now we will setup our basic variables for the demo hello world
+// now we will setup our basic variables for the demo hello 
 var canvas = document.getElementById('canvas'),
 ctx = canvas.getContext('2d'),
 // full screen dimensions
